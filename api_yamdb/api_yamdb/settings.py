4265c0023df85f24207936460f2b0c41a0c071ea
Yaroslav Belyanski <yaroslav.belyanski@yandex.ru>
2023-04-27 11:25:41 +0300

    Add apps

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,6 +22,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'api',
+    'reviews',
 ]
 
 MIDDLEWARE = [
